d934ddfc6fdc9290d4ab0ce99b48fe4b49385fc0
Caarloos007 <cmagrase@myuax.com>
2026-02-27 19:34:07 +0100

    ...

--- burrito.py
+++ burrito.py
@@ -4,13 +4,22 @@ import time
 N = 8 
 
 num_trabajadores = int(input("¿Cuántos trabajadores deseas? "))
-barrier = threading.Barrier(N + num_trabajadores) 
+barrier = threading.Barrier(N + num_trabajadores)
+lock = threading.Lock()
+burrito_counter = 0 
 
 def trabajador(id):
-    for i in range(1, N + 1):
-        print(f"Trabajador {id} está haciendo el burrito {i}...")
+    global burrito_counter
+    while True:
+        with lock:
+            if burrito_counter >= N:
+                break
+            current_burrito = burrito_counter + 1
+            burrito_counter += 1
+        
+        print(f"Trabajador {id} está haciendo el burrito {current_burrito}...")
         time.sleep(5)
-        print(f"Trabajador {id} ha terminado el burrito {i}.")
+        print(f"Trabajador {id} ha terminado el burrito {current_burrito}.")
     
     print("Todos los burritos han sido preparados. Los clientes empiezan a comer.")
     barrier.wait()
